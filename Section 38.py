19ec6d8dcdd64217e132513e045ac8e06c93b037
Alifseen <alifseen@gmail.com>
2024-11-07 11:31:19 +0500

    Extracted specific data from the page.

--- Section 38.py
+++ Section 38.py
@@ -9,6 +9,13 @@ def scrape(url):
     source_data = response.text
     return source_data
 
+## 2. Extract data from the entire page source using YAML and CSS ID Selector
+def specific_data(source):
+    extractor = selectorlib.Extractor.from_yaml_file("files/extract.yaml")  ## Take the source and only store the data that matches the CSS Selector as dictionary
+    value = extractor.extract(source)["data"]  ## Return the value of data store using the key.
+    return value
+
 if __name__ == "__main__":
-    content = scrape(URL)
-    print(content)
+    all_content = scrape(URL)
+    extracted_content = specific_data(all_content)
+    print(extracted_content)
